d1e785b6a8e0ba2bd058d544e6aff49839a8525d
ankit11hab <ankit11hab@gmail.com>
2023-08-27 23:54:17 +0530

    Fetch from content js

--- content.js
+++ content.js
@@ -1,5 +1,35 @@
-console.log(document.body) ;
-let body = document.body;
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  sendResponse({ data: "body" });
+const API_KEY = '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';
+let body = toString(document.body);
+const paragraphs = document.querySelectorAll('p');
+let allParagraphsText = '';
+paragraphs.forEach((paragraph) => {
+  allParagraphsText += paragraph.textContent + '\n';
+});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const APIBody = {
+    model: "text-davinci-003",
+    prompt: "Give the summary of this text " + allParagraphsText,
+    temperature: 1,
+    max_tokens: 100,
+  };
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + API_KEY,
+    },
+    body: JSON.stringify(APIBody),
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      console.log(data.choices[0].text);
+      sendResponse(data.choices[0].text);
+    })
+    .catch(e =>
+      console.log(e.message)
+    )
+  return true;
 });
--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-const API_KEY = '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';
-
-const inputText = '';
-
 document.getElementById("submit").addEventListener(('click'), ()=>{
   handleSubmit();
-})
+});
 
 const handleSubmit = async () => {
   try {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
         console.log(response);
+        const summaryHere = document.getElementById('summaryHere');
+        summaryHere.innerHTML = response ;
       });
     });
-    const APIBody = {
-      model: "text-davinci-003",
-      prompt: "Give the summary of this text " + inputText,
-      temperature: 1,
-      max_tokens: 100,
-    };
-
-    await fetch("https://api.openai.com/v1/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + API_KEY,
-      },
-      body: JSON.stringify(APIBody),
-    })
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        const summaryHere = document.getElementById('summaryHere');
-        summaryHere.innerHTML = data.choices[0].text ;
-        console.log(data.choices[0].text);
-      });
   } catch (err) {
     console.log(err);
   }
 };
-// handleSubmit();
